f7c04702cf2ad88dac46835d7155beaeb63fca6d
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-05-01 07:20:20 +0200

    Stage 3/3

--- Case Converter/task/src/index.js
+++ Case Converter/task/src/index.js
@@ -1,48 +1,67 @@
+// Functions or methods
 function properCase(text) {
-    return text
-        .split(' ')
-        .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ');
+	return text
+		.split(' ')
+		.map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+		.join(' ');
 }
-
-// function sentenceCase(text) {
-// 	var splitStr = text.toLowerCase().split(' ');
-// 	for (var i = 0; i < splitStr.length; i++) {
-// 		splitStr[i] =
-// 			splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-// 	}
-// 	// Directly return the joined string
-// 	return splitStr.join(' ');
-// }
 
 function sentenceCase(theString) {
-    return theString
-        .toLowerCase()
-        .replace(/(^\s*\w|[\.\!\?]\s*\w)/g, function (c) {
-            return c.toUpperCase();
-        });
+	var newString = theString
+		.toLowerCase()
+		.replace(/(^\s*\w|[\.\!\?]\s*\w)/g, function (c) {
+			return c.toUpperCase();
+		});
+	return newString;
 }
 
+function download(filename, text) {
+	let element = document.createElement('a');
+	element.setAttribute(
+		'href',
+		'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+	);
+	element.setAttribute('download', filename);
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+}
+
+// Events
+
 document.getElementById('upper-case').addEventListener('click', function () {
-    document.getElementById('textarea-box').value = document
-        .getElementById('textarea-box')
-        .value.toUpperCase();
+	document.getElementById('textarea-box').value = document
+		.getElementById('textarea-box')
+		.value.toUpperCase();
 });
 
 document.getElementById('lower-case').addEventListener('click', function () {
-    document.getElementById('textarea-box').value = document
-        .getElementById('textarea-box')
-        .value.toLowerCase();
+	document.getElementById('textarea-box').value = document
+		.getElementById('textarea-box')
+		.value.toLowerCase();
 });
 
 document.getElementById('proper-case').addEventListener('click', function () {
-    document.getElementById('textarea-box').value = properCase(
-        document.getElementById('textarea-box').value
-    );
+	document.getElementById('textarea-box').value = properCase(
+		document.getElementById('textarea-box').value
+	);
 });
 
 document.getElementById('sentence-case').addEventListener('click', function () {
-    document.getElementById('textarea-box').value = sentenceCase(
-        document.getElementById('textarea-box').value
-    );
+	document.getElementById('textarea-box').value = sentenceCase(
+		document.getElementById('textarea-box').value
+	);
 });
+
+document
+	.getElementById('save-text-file')
+	.addEventListener('click', function () {
+		document.getElementById('textarea-box').value = download(
+			'text.txt',
+			document.getElementById('textarea-box').value
+		);
+	});
